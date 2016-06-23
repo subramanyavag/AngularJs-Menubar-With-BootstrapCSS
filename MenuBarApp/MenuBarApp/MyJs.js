1cb9a39ff60b5b5e6df61af1a71255e4311352b6
subramanyavag <subramanyavag@gmail.com>
2016-06-23 05:39:25 +0000

    Add files via upload

---
+++ MenuBarApp/MenuBarApp/MyJs.js
@@ -0,0 +1,33 @@
+﻿
+var app = angular.module('menuApp', ['ngRoute']);
+
+app.controller('menuCntrl', function ($scope) {
+    $scope.message = "Welcome ";
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when('/home', {
+        templateUrl: 'Pages/Home.html',
+        controller: 'homeController'
+    })
+    .when('/about', {
+        templateUrl: 'Pages/About.html',
+        controller: 'aboutController'
+    })
+     .when('/contact', {
+         templateUrl: 'Pages/Contact.html',
+         controller: 'contactController'
+     });
+});
+
+app.controller('homeController', function ($scope) {
+    $scope.message = "Home Page ";
+});
+
+app.controller('aboutController', function ($scope) {
+    $scope.message = "About Page ";
+});
+app.controller('contactController', function ($scope) {
+    $scope.message = "Contact Page ";
+});
